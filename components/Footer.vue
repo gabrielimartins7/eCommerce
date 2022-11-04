2838f32509cc8941dbdf156168878b4677415c84
gabrielimartins7 <martins.gabrieli.07.almeida@outlook.com>
2022-11-04 14:50:37 -0300

    Criando o footer

---
+++ components/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card tile flat class="pa-md-10 pa-5 text-center" color="surface">
+    <h2 class="text-md-h6 font-weight-bold">Ecommerce</h2>
+    <dev class="text-center">
+      <v-btn
+      v-for="(b,i) in sm"
+      :key="`socialMedia-${i}`"
+      color="surface"
+      class="mr-2"
+      fab
+      depressed
+      >
+        <v-icon>{{b.icon}}</v-icon>
+      </v-btn>
+    </dev>
+  </v-card>
+</template>
+
+<script>
+export default {
+data(){
+  return {
+    sm:[
+      {
+        icon: 'mdi-facebook',
+        link: '#',
+      },
+      {
+        icon: 'mdi-instagram',
+        link: '#',
+      },
+      {
+        icon: 'mdi-twitter',
+        link: '#',
+      },
+      {
+        icon: 'mdi-youtube',
+        link: '#',
+      }
+    ]
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
